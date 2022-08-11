299b0c90c22911234243081f5eee805b7bcca655
quangsi <77560605+quangsiDev@users.noreply.github.com>
2022-08-11 11:40:42 +0700

    demo state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import DemoClass from "./DemoComponent/DemoClass";
 import DemoFunction from "./DemoComponent/DemoFunction";
 import Ex_Layout from "./Ex_Layout/Ex_Layout";
 import Data_Binding from "./Data_Binding/Data_Binding";
+import Event_Binding from "./Event_Binding/Event_Binding";
+import Conditional_Rendering from "./Conditional_Rendering/Conditional_Rendering";
+import Demo_State from "./Demo_State/Demo_State";
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
       {/* <DemoFunction /> */}
       {/* <Ex_Layout /> */}
 
-      <Data_Binding />
+      {/* <Data_Binding /> */}
+      {/* <Event_Binding /> */}
+      {/* <Conditional_Rendering /> */}
+      <Demo_State />
     </div>
   );
 }
--- src/Data_Binding/Data_Binding.jsx
+++ src/Data_Binding/Data_Binding.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 
 export default class Data_Binding extends Component {
+  title = "Vui quá";
   render() {
+    let cardStyle = { width: "18rem" };
+    let imgSrc = "https://picsum.photos/200";
     return (
       <div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            className="card-img-top"
-            src=".../100px180/"
-            alt="Card image cap"
-          />
+        <div className="card" style={cardStyle}>
+          <img className="card-img-top" src={imgSrc} alt="Card image cap" />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
+            <h5 className="card-title">{this.title}</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
