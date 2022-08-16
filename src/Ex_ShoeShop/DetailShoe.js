a4f527bfe66a2d80a36e29fb4270c9773f3be625
quangsi <77560605+quangsiDev@users.noreply.github.com>
2022-08-16 09:00:15 +0700

    update

--- src/Ex_ShoeShop/DetailShoe.js
+++ src/Ex_ShoeShop/DetailShoe.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 
 export default class DetailShoe extends Component {
   render() {
-    return <div>DetailShoe</div>;
+    let { image } = this.props.detailShoe;
+    return (
+      <div className="container p-5">
+        <div className="row">
+          <div className="col-4">
+            <img src={image} alt="" className="w-100" />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
